59181e1be1238181c2a363142ce4c22ff2bf3f77
PoojaAshwath <poojaashwath1709@gmail.com>
2024-03-21 17:14:34 +0530

    ConfiguredReactRouter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import MyApp from './MyApp';
 import React from 'react';
 import { useEffect, useState } from "react";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Context from './MyApp/context';
 
 function App() {
+  const router = createBrowserRouter([{"path": "/", "element": <MyApp />},{"path": "/context", "element": <Context /> }])
+
   const [festival,setFestival] = useState("diwali");
   return (
     <React.Fragment>
-    <MyApp  festival={festival} setFestival={setFestival}/>
+    {/* <MyApp  festival={festival} setFestival={setFestival}/> */}
+    <RouterProvider router={router} />
     </React.Fragment>
   );
 }
